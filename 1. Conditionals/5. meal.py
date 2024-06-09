0aa3bb13f27d59cb2363ee0dfafafb551b48176c
Genesis Varelli <118129965+GenesisVarelli@users.noreply.github.com>
2024-06-09 11:18:54 +0100

    Update 5. meal.py

--- 1. Conditionals/5. meal.py
+++ 1. Conditionals/5. meal.py
@@ -1,1 +1,25 @@
+def main():
+    #prompt user for  time
+    mealtime = input("What time is it? ").strip()
 
+    time = convert(mealtime)
+
+    if 7 <= time <= 8:
+        print('breakfast time')
+    elif 12 <= time <= 13:
+        print('lunch time')
+    elif 18 <= time <= 19:
+        print('dinner time')
+    else:
+        pass
+
+def convert(time):
+     # Get hours and minutes
+     hours, minutes = time.split(":")
+     # Convert time into a float number
+     minutes_new = float(minutes) / 60
+    # Return the result to main function
+     return float(hours) + minutes_new
+
+if __name__ == "__main__":
+    main()
